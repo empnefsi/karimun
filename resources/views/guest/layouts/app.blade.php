b93d6ff7e3bab09b919c5a56fde1790bf64020e2
rezzaldy <73360240+rezzaldy@users.noreply.github.com>
2021-05-24 14:15:56 +0700

    :truck: moving asset directory

--- resources/views/guest/layouts/app.blade.php
+++ resources/views/guest/layouts/app.blade.php
@@ -9,16 +9,16 @@
         <link href="https://fonts.googleapis.com/css?family=Alex+Brush" rel="stylesheet">
 
         {{-- guest CSS --}}
-        <link href="{{ asset('resources') }}/css/animate.css" rel="stylesheet">
-        <link href="{{ asset('resources') }}/css/aos.css" rel="stylesheet">
-        <link href="{{ asset('resources') }}/css/flaticon.css" rel="stylesheet">
-        <link href="{{ asset('resources') }}/css/icomoon.css" rel="stylesheet">
-        <link href="{{ asset('resources') }}/css/ionicons.min.css" rel="stylesheet">
-        <link href="{{ asset('resources') }}/css/magnific.popup.css" rel="stylesheet">
-        <link href="{{ asset('resources') }}/css/open-iconic-bootstrap.min.css" rel="stylesheet">
-        <link href="{{ asset('resources') }}/css/owl.carousel.min.css" rel="stylesheet">
-        <link href="{{ asset('resources') }}/css/owl.theme.default.min.css" rel="stylesheet">
-        <link href="{{ asset('resources') }}/css/style.css" rel="stylesheet">
+        <link href="{{ asset('guest') }}/css/animate.css" rel="stylesheet">
+        <link href="{{ asset('guest') }}/css/aos.css" rel="stylesheet">
+        <link href="{{ asset('guest') }}/css/flaticon.css" rel="stylesheet">
+        <link href="{{ asset('guest') }}/css/icomoon.css" rel="stylesheet">
+        <link href="{{ asset('guest') }}/css/ionicons.min.css" rel="stylesheet">
+        <link href="{{ asset('guest') }}/css/magnific.popup.css" rel="stylesheet">
+        <link href="{{ asset('guest') }}/css/open-iconic-bootstrap.min.css" rel="stylesheet">
+        <link href="{{ asset('guest') }}/css/owl.carousel.min.css" rel="stylesheet">
+        <link href="{{ asset('guest') }}/css/owl.theme.default.min.css" rel="stylesheet">
+        <link href="{{ asset('guest') }}/css/style.css" rel="stylesheet">
     </head>
 
     <body>
@@ -30,20 +30,20 @@
 
         {{-- guest JS --}}
         <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
-        <script src="{{ asset('resources') }}/guest/js/aos.js"></script>
-        <script src="{{ asset('resources') }}/guest/js/bootstrap-datepicker.js"></script>
-        <script src="{{ asset('resources') }}/guest/js/bootstrap.min.js"></script>
-        <script src="{{ asset('resources') }}/guest/js/google-map.js"></script>
-        <script src="{{ asset('resources') }}/guest/js/jquery-migrate-3.0.1.min.js"></script>
-        <script src="{{ asset('resources') }}/guest/js/jquery-animateNumber.js"></script>
-        <script src="{{ asset('resources') }}/guest/js/jquery.easing.1.3.js"></script>
-        <script src="{{ asset('resources') }}/guest/js/jquery.magnific-popup.min.js"></script>
-        <script src="{{ asset('resources') }}/guest/js/jquery.min.js"></script>
-        <script src="{{ asset('resources') }}/guest/js/jquery.stellar.min.js"></script>
-        <script src="{{ asset('resources') }}/guest/js/jquery.waypoint.min.js"></script>
-        <script src="{{ asset('resources') }}/guest/js/main.js"></script>
-        <script src="{{ asset('resources') }}/guest/js/owl.carousel.min.js"></script>
-        <script src="{{ asset('resources') }}/guest/js/popper.min.js"></script>
-        <script src="{{ asset('resources') }}/guest/js/scrollax.min.js"></script>
+        <script src="{{ asset('guest') }}/js/aos.js"></script>
+        <script src="{{ asset('guest') }}/js/bootstrap-datepicker.js"></script>
+        <script src="{{ asset('guest') }}/js/bootstrap.min.js"></script>
+        <script src="{{ asset('guest') }}/js/google-map.js"></script>
+        <script src="{{ asset('guest') }}/js/jquery-migrate-3.0.1.min.js"></script>
+        <script src="{{ asset('guest') }}/js/jquery-animateNumber.js"></script>
+        <script src="{{ asset('guest') }}/js/jquery.easing.1.3.js"></script>
+        <script src="{{ asset('guest') }}/js/jquery.magnific-popup.min.js"></script>
+        <script src="{{ asset('guest') }}/js/jquery.min.js"></script>
+        <script src="{{ asset('guest') }}/js/jquery.stellar.min.js"></script>
+        <script src="{{ asset('guest') }}/js/jquery.waypoint.min.js"></script>
+        <script src="{{ asset('guest') }}/js/main.js"></script>
+        <script src="{{ asset('guest') }}/js/owl.carousel.min.js"></script>
+        <script src="{{ asset('guest') }}/js/popper.min.js"></script>
+        <script src="{{ asset('guest') }}/js/scrollax.min.js"></script>
     </body>
 </html>
