6206ce5ff8525e43d477d494c3170a0cc3c1ed7d
Hilmi Wicaksono <hilmiwicaksonop@gmail.com>
2021-05-24 13:23:08 +0700

    :truck: picked web route from news

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,17 @@ Route::group(['middleware' => 'auth'], function () {
 	 Route::get('icons', function () {return view('pages.icons');})->name('icons'); 
 	 Route::get('table-list', function () {return view('pages.tables');})->name('table');
 	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'App\Http\Controllers\ProfileController@password']);
+
+	Route::resource('news', 'App\Http\Controllers\NewsController', ['except' => ['show']]);
+	
+	Route::resource('packages', 'App\Http\Controllers\PackageController', ['except' => ['show']]);
+
+	Route::get('/destinationmanagement', 'App\Http\Controllers\DestinationController@index')->name('destinationmanagement');
+	Route::post('destinationForms', [App\Http\Controllers\DestinationController::class, 'store'])->name('destinationForms');
+	Route::get('/adddestination',[App\Http\Controllers\DestinationController::class,'show']);
+	Route::get('deletedestination/{slug}', [App\Http\Controllers\DestinationController::class,'destroy']);
+	Route::get('editdestination/{slug}', [App\Http\Controllers\DestinationController::class, 'showEdit']);
+	Route::post('/destinationFormsEdit', [App\Http\Controllers\DestinationController::class, 'update'])->name('destinationFormsEdit');
 });
 
 
