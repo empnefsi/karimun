61ccada09a570dcb30def19ba26fa481202ce046
Hilmi Wicaksono <hilmiwicaksonop@gmail.com>
2021-05-25 16:39:20 +0700

    :truck: moved route admin/ and admin/home into group

--- routes/web.php
+++ routes/web.php
@@ -17,18 +17,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('admin', function(){ return view('welcome'); });
-Route::get('/admin/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 Auth::routes();
 
-Route::group(['prefix' => 'admin', 'middleware' => 'auth'],  function () {
-	Route::resource('user', 'App\Http\Controllers\UserController', ['except' => ['show']]);
-	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'App\Http\Controllers\ProfileController@edit']);
-	Route::put('profile', ['as' => 'profile.update', 'uses' => 'App\Http\Controllers\ProfileController@update']);
-	Route::get('upgrade', function () {return view('pages.upgrade');})->name('upgrade'); 
-	 Route::get('map', function () {return view('pages.maps');})->name('map');
-	 Route::get('icons', function () {return view('pages.icons');})->name('icons'); 
-	 Route::get('table-list', function () {return view('pages.tables');})->name('table');
-	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'App\Http\Controllers\ProfileController@password']);
+Route::group(['prefix' => 'admin'],  function () {
+	Route::get('/', function(){ return view('welcome'); });
+	Route::get('home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+	Route::group(['middleware' => 'auth'], function(){
+		Route::resource('user', 'App\Http\Controllers\UserController', ['except' => ['show']]);
+		Route::get('profile', ['as' => 'profile.edit', 'uses' => 'App\Http\Controllers\ProfileController@edit']);
+		Route::put('profile', ['as' => 'profile.update', 'uses' => 'App\Http\Controllers\ProfileController@update']);
+		Route::get('upgrade', function () {return view('pages.upgrade');})->name('upgrade'); 
+		Route::get('map', function () {return view('pages.maps');})->name('map');
+		Route::get('icons', function () {return view('pages.icons');})->name('icons'); 
+		Route::get('table-list', function () {return view('pages.tables');})->name('table');
+		Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'App\Http\Controllers\ProfileController@password']);
+	});
 });
